003cf6f85b5bb599aa4eb84f0411d38009e8c178
WangRuiGuo2025 <pigeons2023@hotmail.com>
2025-11-28 17:16:39 +0800

    添加几个收藏的网站

--- src/components/WebCollection.vue
+++ src/components/WebCollection.vue
@@ -8,6 +8,8 @@
         <div class="list">
             <a class="a1" href="https://www.sdut.edu.cn/">山东理工大学</a>
             <a class="a1" href="https://jsjxy.sdut.edu.cn/">计算机科学与技术学院</a>
+            <a class="a1" href="https://ehall.sdut.edu.cn/new/ehall.html">信息门户</a>
+            <a class="a1" href="http://211.64.28.123/jwglxt/xtgl/login_slogin.html">教务系统</a>
             <a class="a1" href="https://www.chsi.com.cn/">学信网</a>
             <a class="a1" href="https://www.lanzoui.com/">网盘</a>
             <a class="a1" href="https://picui.cn/">图床</a>
@@ -15,6 +17,8 @@
             <a class="a1" href="https://peanutdl.com/zh">视频</a>
             <a class="a1" href="https://github.akams.cn/">Github加速下载</a>
             <a class="a1" href="https://www.doubao.com/chat/">豆包</a>
+            <a class="a1" href="https://aistudio.google.com/">Gemini</a>
+
         </div>
     </div>
 </template>
